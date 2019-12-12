fac0236464d71c4d4fb71a45729d347173c1d552
unknown <33382781+YuKimura45z@users.noreply.github.com>
2019-12-12 21:20:33 +0900

    [update]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+import { MediaChange, MediaObserver } from "@angular/flex-layout";
+import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'cosmoscan';
+  forms: {
+    search: string;
+  };
+  drawerMode$: Observable<string>;
+  drawerOpened$: Observable<boolean>;
+
+  constructor(private router: Router, mediaObserver: MediaObserver) {
+    this.forms = {
+      search: ""
+    };
+    this.drawerMode$ = mediaObserver.media$.pipe(
+      map(change => (change.mqAlias === "xs" ? "over" : "side"))
+    );
+
+    this.drawerOpened$ = mediaObserver.media$.pipe(
+      map(change => (change.mqAlias === "xs" ? false : true))
+    );
+  }
+
+  async submit() {
+    await this.router.navigate([""], { queryParams: { q: this.forms.search } });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
