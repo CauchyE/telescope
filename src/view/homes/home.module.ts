3410c62c6d0edda617146f4e04399c30f5e13637
bcts369 <41810380+bcts369@users.noreply.github.com>
2020-07-01 17:00:20 +0800

    add module

---
+++ src/view/homes/home.module.ts
@@ -0,0 +1,13 @@
+export class HomesModule { }
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component'
+import { MaterialModule } from '@view/material.module';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [HomeComponent]
+})
+export class HomeModule { }
