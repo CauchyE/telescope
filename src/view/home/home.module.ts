9f9d739c0dc51a18039a46fa370cc94852d56d53
bcts369 <41810380+bcts369@users.noreply.github.com>
2020-07-01 17:32:41 +0800

    [wip] #15 change dir

--- src/view/home/home.module.ts
+++ src/view/home/home.module.ts
@@ -2,7 +2,7 @@ export class HomesModule { }
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home.component'
 import { MaterialModule } from '@view/material.module';
 
 @NgModule({
