0f007407a9cfc602942feff400413c8984c857b2
bcts369 <41810380+bcts369@users.noreply.github.com>
2020-07-02 15:34:51 +0800

    change app module

--- src/app/validators/validators.module.ts
+++ src/app/validators/validators.module.ts
@@ -5,9 +5,10 @@ import { ValidatorsRoutingModule } from './validators-routing.module';
 import { ValidatorsViewModule } from '@view/validators/validators.module';
 import { ValidatorsComponent } from './validators.component';
 import { ValidatorComponent } from './validator/validator.component';
+import { ValidatorModule } from '@view/validators/validator/validator.module';
 
 @NgModule({
   declarations: [ValidatorsComponent, ValidatorComponent],
-  imports: [CommonModule, ValidatorsRoutingModule, ValidatorsViewModule],
+  imports: [CommonModule, ValidatorsRoutingModule, ValidatorsViewModule, ValidatorModule],
 })
-export class AppValidatorsModule {}
+export class AppValidatorsModule { }
