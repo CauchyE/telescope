79b3d90d3b7ef7cee03f0d04aea20deab2fbfbea
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2020-08-19 08:05:38 +0900

    update

--- src/app/keys/keys.component.ts
+++ src/app/keys/keys.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { Key } from '@model/keys/key.model';
+import { KeyService } from '@model/keys/key.service';
 
 @Component({
   selector: 'app-keys',
   templateUrl: './keys.component.html',
-  styleUrls: ['./keys.component.css']
+  styleUrls: ['./keys.component.css'],
 })
 export class KeysComponent implements OnInit {
+  keys$: Observable<Key[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly key: KeyService) {
+    this.keys$ = from(this.key.keys());
   }
 
+  ngOnInit(): void {}
 }
