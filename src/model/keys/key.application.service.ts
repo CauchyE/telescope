c0fa8fd173d7b3645eddc90702a7854744b2b053
kourin <kourin1996@gmail.com>
2020-08-29 10:31:39 +0000

    feat: check for unique id

--- src/model/keys/key.application.service.ts
+++ src/model/keys/key.application.service.ts
@@ -20,8 +20,15 @@ export class KeyApplicationService {
   }
 
   async create(id: string, type: KeyType, privateKey: string) {
-    const dialogRef = this.loadingDialog.open('Creating');
+    const key = await this.key.get(id);
+    if (key !== undefined) {
+      this.snackBar.open('ID is already used', undefined, {
+        duration: 6000,
+      });
+      return;
+    }
 
+    const dialogRef = this.loadingDialog.open('Creating');
     try {
       await this.key.set(id, type, privateKey);
     } catch {
