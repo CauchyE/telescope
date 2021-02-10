2b69cf42369a41e4d33c9e7310a45f8ed69f2cb2
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-02-10 20:06:40 +0900

    update

--- src/model/keys/key-select-dialog.service.ts
+++ src/model/keys/key-select-dialog.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { NavigationEnd, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { KeySelectDialogComponent } from '@view/keys/key-select-dialog/key-select-dialog.component';
 import { first } from 'rxjs/operators';
 import { Key } from './key.model';
@@ -12,17 +12,11 @@ import { KeyStoreService } from './key.store.service';
 })
 export class KeySelectDialogService {
   constructor(
+    private readonly router: Router,
     private readonly dialog: MatDialog,
     private readonly key: KeyService,
     private readonly keyStore: KeyStoreService,
-    private readonly router: Router,
-  ) {
-    this.router.events.subscribe((event) => {
-      if (!(event instanceof NavigationEnd)) {
-        return;
-      }
-    });
-  }
+  ) {}
 
   async open() {
     const keys = await this.key.list();
