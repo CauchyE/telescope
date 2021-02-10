a786fbf864cd7bb42fa8d73cf6873870cfdfd41e
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-02-10 16:07:27 +0900

    update

--- src/app/cosmos/bank/bank.module.ts
+++ src/app/cosmos/bank/bank.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { BankRoutingModule } from './bank-routing.module';
 import { SendComponent } from './send/send.component';
 import { BankComponent } from './bank.component';
+import { BankModule } from '@view/cosmos/bank/bank.module';
 import { SendModule } from '@view/cosmos/bank/send/send.module';
 
 @NgModule({
   declarations: [BankComponent, SendComponent],
-  imports: [CommonModule, BankRoutingModule, SendModule],
+  imports: [CommonModule, BankRoutingModule, BankModule, SendModule],
 })
 export class AppBankModule {}
