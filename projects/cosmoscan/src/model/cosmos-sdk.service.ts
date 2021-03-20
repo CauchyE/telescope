e95babd7aa7373aee66268e1e65c8399932e2746
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-03-21 01:11:47 +0900

    feat: default-config

--- projects/cosmoscan/src/model/cosmos-sdk.service.ts
+++ projects/cosmoscan/src/model/cosmos-sdk.service.ts
@@ -10,8 +10,8 @@ import * as config from '../config.json';
 export class CosmosSDKService {
   restURL$: BehaviorSubject<string>;
   websocketURL$: BehaviorSubject<string>;
-  chainID$: BehaviorSubject<string>
-  sdk$: Observable<{ rest: cosmosclient.CosmosSDK, websocket: cosmosclient.CosmosSDK }>;
+  chainID$: BehaviorSubject<string>;
+  sdk$: Observable<{ rest: cosmosclient.CosmosSDK; websocket: cosmosclient.CosmosSDK }>;
 
   constructor() {
     if (
@@ -29,19 +29,21 @@ export class CosmosSDKService {
         valPub: config.bech32_prefix?.val_addr,
         consAddr: config.bech32_prefix?.cons_addr,
         consPub: config.bech32_prefix?.cons_addr,
-      }
+      };
     }
 
-    this.restURL$ = new BehaviorSubject('')
-    this.websocketURL$ = new BehaviorSubject('')
-    this.chainID$ = new BehaviorSubject('')
-    this.sdk$ = combineLatest([this.restURL$, this.websocketURL$, this.chainID$]).pipe(map(([restURL, websocketURL, chainID]) => ({
-      rest: new cosmosclient.CosmosSDK(restURL, chainID),
-      websocket: new cosmosclient.CosmosSDK(websocketURL, chainID)
-    })))
+    this.restURL$ = new BehaviorSubject(`${location.protocol}//${location.hostname}:1317`);
+    this.websocketURL$ = new BehaviorSubject(`${location.protocol.replace('http', 'ws')}://${location.hostname}:26657`);
+    this.chainID$ = new BehaviorSubject('cosmoshub-3');
+    this.sdk$ = combineLatest([this.restURL$, this.websocketURL$, this.chainID$]).pipe(
+      map(([restURL, websocketURL, chainID]) => ({
+        rest: new cosmosclient.CosmosSDK(restURL, chainID),
+        websocket: new cosmosclient.CosmosSDK(websocketURL, chainID),
+      })),
+    );
   }
 
   sdk() {
-    return  this.sdk$.pipe(first()).toPromise()
+    return this.sdk$.pipe(first()).toPromise();
   }
 }
