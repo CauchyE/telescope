8252d9e4ecbbdaa2ba08dd1c8a7d89c3785c6e5d
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-07-02 11:05:14 +0900

    fix: polyfill.ts

--- projects/cosmoscan/src/polyfills.ts
+++ projects/cosmoscan/src/polyfills.ts
@@ -63,3 +63,4 @@ import 'zone.js'; // Included with Angular CLI.
 
 (window as any).global = window;
 global.Buffer = global.Buffer || require('buffer').Buffer;
+global.process = require('process');
