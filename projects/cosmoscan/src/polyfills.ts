681c694280fcea234f51374ebaff22cae5966925
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-07-02 10:56:56 +0900

    fix: polyfill.ts

--- projects/cosmoscan/src/polyfills.ts
+++ projects/cosmoscan/src/polyfills.ts
@@ -55,9 +55,11 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
-
+import 'zone.js'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+(window as any).global = window;
+global.Buffer = global.Buffer || require('buffer').Buffer;
