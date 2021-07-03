89d6a4b7ce342be8528be0e132e581eae8553018
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-07-03 21:28:00 +0900

    feat: config

---
+++ projects/cosmoscan/src/model/config.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+export type Config = {
+  restURL: string;
+  websocketURL: string;
+  chainID: string;
+  bech32Prefix: {
+    accAddr: string;
+    accPub: string;
+    valAddr: string;
+    valPub: string;
+    consAddr: string;
+    consPub: string;
+  };
+  extension: {
+    navigations: {
+      name: string;
+      link: string;
+    }[];
+    messageActions: string[];
+  };
+};
+
+declare const config: Config;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigService {
+  config: Config;
+  constructor() {
+    this.config = config;
+  }
+}
