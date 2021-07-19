66f0a51c0ea176a4c1b758bd2caa7e3c02414c4c
Senna46 <Senna46@users.noreply.github.com>
2021-07-19 17:22:28 +0900

    fix: pub_key unpackAny

--- projects/cosmoscan/src/view/accounts/account/account.component.ts
+++ projects/cosmoscan/src/view/accounts/account/account.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { cosmos } from 'cosmos-client';
+import { cosmos, cosmosclient } from 'cosmos-client';
 
 @Component({
   selector: 'view-account',
@@ -19,9 +19,9 @@ export class AccountComponent implements OnInit, OnChanges {
 
   txColumnKeys = ['height', 'txhash', 'timestamp', 'gas_wanted', 'gas_used'];
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   ngOnChanges() {
     delete this.baseAccount;
@@ -29,11 +29,14 @@ export class AccountComponent implements OnInit, OnChanges {
     if (this.account instanceof cosmos.auth.v1beta1.BaseAccount) {
       this.baseAccount = this.account;
       // Todo: fix pub_key zero issue
-      console.log(this.baseAccount.pub_key?.value);
-      if (this.baseAccount.pub_key?.value) {
-        this.publicKey = Buffer.from(this.baseAccount.pub_key?.value).toString('base64');
-        console.log(this.publicKey);
+      console.log(cosmosclient.codec.unpackAny(this.baseAccount.pub_key));
+      const publicKey = cosmosclient.codec.unpackAny(this.baseAccount.pub_key);
+
+      if (!(publicKey instanceof cosmos.crypto.secp256k1.PubKey)) {
+        throw Error('hoge');
       }
+      this.publicKey = Buffer.from(publicKey.key).toString('hex');
+      console.log(this.publicKey);
     }
   }
 }
