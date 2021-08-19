1dc1688a502ee3db1fcf713a1c6395a368270522
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-08-19 18:59:36 +0900

    feat: latest block initial value 1

--- projects/cosmoscan/src/app/home/blocks/blocks.component.ts
+++ projects/cosmoscan/src/app/home/blocks/blocks.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { websocket } from 'cosmos-client';
+import { rest, websocket } from 'cosmos-client';
+import { InlineResponse20031 } from 'cosmos-client/esm/openapi';
 import { CosmosSDKService } from 'projects/cosmoscan/src/model/cosmos-sdk.service';
 import { Observable } from 'rxjs';
-import { first, map, scan } from 'rxjs/operators';
+import { first, map, mergeMap, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-blocks',
@@ -11,16 +12,31 @@ import { first, map, scan } from 'rxjs/operators';
   styleUrls: ['./blocks.component.css'],
 })
 export class BlocksComponent implements OnInit {
+  initialBlocks$?: Observable<InlineResponse20031>;
   latestBlocks$?: Observable<websocket.RequestSchema[] | websocket.ResponseSchema[]>;
 
   constructor(private route: ActivatedRoute, private cosmosSDK: CosmosSDKService) {
+    const initial = this.cosmosSDK.sdk$.pipe(
+      mergeMap(sdk => rest.cosmos.tendermint.getLatestBlock(sdk.rest).then((res) => res.data))
+    );
+
+    //暫定的に最新1つを表示
+    this.initialBlocks$ = initial;
+
+    //for (let i = 0; i < 20; i++) {
+      //this.initialBlocks$ = this.cosmosSDK.sdk$.pipe(
+        //mergeMap(sdk => rest.cosmos.tendermint.getBlockByHeight(sdk.rest, BigInt(2650) - BigInt(i)).then((res) => res.data)),
+        //scan<any>((all, current) => [current, ...all], []),
+      //);
+    //}
+
     this.cosmosSDK.websocketURL$.pipe(first()).subscribe((websocketURL) => {
       const ws = websocket.connect(websocketURL);
       ws.next({
         id: '1',
         jsonrpc: '2.0',
         method: 'subscribe',
-        params: ["tm.event = 'NewBlock'"],
+        params: [`tm.event = 'NewBlock'`],
       });
       this.latestBlocks$ = ws.asObservable().pipe(
         scan<any>((all, current) => [current, ...all], []),
@@ -41,10 +57,10 @@ export class BlocksComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    //一時的にデバッグ用に追加
-    this.latestBlocks$?.subscribe((latestBlocks) => {
-      console.log('latestBlocks');
-      console.log(latestBlocks);
+    this.initialBlocks$?.subscribe((initialBlocks) => {
+      //一時的にデバッグ用に追加
+      console.log('initialBlocks');
+      console.log(initialBlocks);
     });
   }
 }
--- projects/cosmoscan/src/view/home/blocks/blocks.component.ts
+++ projects/cosmoscan/src/view/home/blocks/blocks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { websocket } from 'cosmos-client';
+import { InlineResponse20031 } from 'cosmos-client/esm/openapi';
 
 @Component({
   selector: 'view-blocks',
@@ -7,6 +8,8 @@ import { websocket } from 'cosmos-client';
   styleUrls: ['./blocks.component.css'],
 })
 export class BlocksComponent implements OnInit {
+  @Input()
+  initialBlocks?: InlineResponse20031 | null | undefined;
   @Input()
   latestBlocks?: websocket.ResponseSchema[] | null | undefined;
 
