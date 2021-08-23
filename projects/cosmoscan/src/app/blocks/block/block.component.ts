5454a610997e31942c12ceb4705755d6f820a4a8
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-08-23 16:30:50 +0900

    feat: validatorsets by block height

--- projects/cosmoscan/src/app/blocks/block/block.component.ts
+++ projects/cosmoscan/src/app/blocks/block/block.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { rest } from 'cosmos-client';
+import { CosmosBaseTendermintV1beta1GetValidatorSetByHeightResponse } from 'cosmos-client/cjs/openapi/api';
 import { InlineResponse20032 } from 'cosmos-client/esm/openapi';
 import { CosmosSDKService } from 'projects/cosmoscan/src/model/cosmos-sdk.service';
 import { combineLatest, Observable } from 'rxjs';
@@ -14,13 +15,22 @@ import { map, mergeMap } from 'rxjs/operators';
 export class BlockComponent implements OnInit {
   blockHeight$: Observable<string>;
   block$: Observable<InlineResponse20032>;
+  validatorsets$: Observable<CosmosBaseTendermintV1beta1GetValidatorSetByHeightResponse>;
 
   constructor(private route: ActivatedRoute, private cosmosSDK: CosmosSDKService) {
     this.blockHeight$ = this.route.params.pipe(map((params) => params.block_height));
     this.block$ = combineLatest([this.cosmosSDK.sdk$, this.blockHeight$]).pipe(
       mergeMap(([sdk, height]) => rest.cosmos.tendermint.getBlockByHeight(sdk.rest, BigInt(height)).then((res) => res.data)),
     );
+    this.validatorsets$ = combineLatest([this.cosmosSDK.sdk$, this.blockHeight$]).pipe(
+      mergeMap(([sdk, height]) => rest.cosmos.tendermint.getValidatorSetByHeight(sdk.rest, height).then((res) => res.data)),
+    );
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.validatorsets$?.subscribe((validatorsets) => {
+      console.log('validatorsets');
+      console.log(validatorsets);
+   });
+  }
 }
--- projects/cosmoscan/src/view/blocks/block/block.component.ts
+++ projects/cosmoscan/src/view/blocks/block/block.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CosmosBaseTendermintV1beta1GetValidatorSetByHeightResponse } from 'cosmos-client/cjs/openapi/api';
 import { InlineResponse20032 } from 'cosmos-client/esm/openapi';
 
 @Component({
@@ -9,6 +10,8 @@ import { InlineResponse20032 } from 'cosmos-client/esm/openapi';
 export class BlockComponent implements OnInit {
   @Input()
   block?: InlineResponse20032 | null;
+  @Input()
+  validatorsets?: CosmosBaseTendermintV1beta1GetValidatorSetByHeightResponse | null;
 
   constructor() { }
 
