389d64dc046fd7cbbefb40e920754fbbfe4c57aa
taro04 <75844498+taro04@users.noreply.github.com>
2021-09-17 22:56:19 +0900

    feat: pulldown

--- projects/main/src/app/views/home/txs/txs.component.ts
+++ projects/main/src/app/views/home/txs/txs.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { websocket } from 'cosmos-client';
 import { CosmosTxV1beta1GetTxsEventResponseTxResponses } from 'cosmos-client/esm/openapi';
+import { config } from 'process';
+import { Config, ConfigService } from './../../../models/config.service';
 
 @Component({
   selector: 'view-txs',
@@ -12,8 +14,15 @@ export class TxsComponent implements OnInit {
   initialTxs?: CosmosTxV1beta1GetTxsEventResponseTxResponses[] | undefined | null;
   @Input()
   latestTxs?: websocket.ResponseSchema[] | undefined | null;
+  //@Input()
+  //config?: Config
 
-  constructor() { }
+  constructor(
+    private readonly config: ConfigService
+  ) { }
 
   ngOnInit(): void { }
+
+  tx_group = this.config.config.extension?.messageModules
+  selected_tx_group = this.tx_group?.[0]
 }
