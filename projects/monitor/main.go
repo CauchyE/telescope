2b5b49709f39fd6347c3d0b6cdad4aadf45d1a57
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-09-29 13:50:09 +0900

    fix: cronFlag

--- projects/monitor/main.go
+++ projects/monitor/main.go
@@ -65,24 +65,12 @@ func serveCmd() *cobra.Command {
 			}
 
       // update 1 min
-      // if cronFlag {
-			// 	c := cron.New()
-			// 	c.AddFunc("0 * * * *", func() {
-			// 		monitor.Health()
-			// 	})
-			// 	c.AddFunc("0 0 * * *", func() {
-			// 		now := time.Now()
-			// 		monitor.Fetch(&now)
-			// 	})
-
-			// 	c.Start()
-			// }
-			if cronFlag {
+      if cronFlag {
 				c := cron.New()
-				c.AddFunc("* * * * *", func() {
+				c.AddFunc("0 * * * *", func() {
 					monitor.Health()
 				})
-				c.AddFunc("* * * * *", func() {
+				c.AddFunc("0 0 * * *", func() {
 					now := time.Now()
 					monitor.Fetch(&now)
 				})
