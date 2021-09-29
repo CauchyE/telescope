981e09a9d65486b7e522bb6392138e7b257a5a1c
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-09-29 17:55:03 +0900

    fix: chore

--- projects/main/src/app/pages/monitor/monitor.component.ts
+++ projects/main/src/app/pages/monitor/monitor.component.ts
@@ -10,12 +10,7 @@ export class MonitorComponent implements OnInit {
   data: Promise<Data[]>;
 
   constructor(private readonly monitor: MonitorService) {
-    const today = new Date(); // 変数が呼ばれた時の日時がDate型で取得できる
-    const year = today.getFullYear(); // 2019とかを取得できる
-    const month = today.getMonth(); // 月は0~11の値で管理されているというトラップ
-    const date = today.getDate(); // 日付は普通に1~の数字で管理されている
-    this.data = this.monitor.list(year, month, date, 1);
-    this.data.then((d) => console.log(d));
+    this.data = this.monitor.list(2021, 9, 28, 1);
   }
 
   ngOnInit(): void {}
