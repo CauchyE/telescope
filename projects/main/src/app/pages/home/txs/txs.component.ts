da4ab2ea8dddb51025fda94362da33d519808166
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-10-14 10:08:55 +0900

    fix: lint

--- projects/main/src/app/pages/home/txs/txs.component.ts
+++ projects/main/src/app/pages/home/txs/txs.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { rest, websocket } from 'cosmos-client';
 import { CosmosTxV1beta1GetTxsEventResponseTxResponses } from 'cosmos-client/esm/openapi/api';
-import { CosmosSDKService } from 'projects/main/src/app/models/cosmos-sdk.service';
 import { ConfigService } from 'projects/main/src/app/models/config.service';
+import { CosmosSDKService } from 'projects/main/src/app/models/cosmos-sdk.service';
 import { BehaviorSubject, combineLatest, Observable, timer } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 
@@ -20,8 +20,12 @@ export class TxsComponent implements OnInit {
   txsTotalCount$: Observable<bigint>;
   selectedTxType$: BehaviorSubject<string> = new BehaviorSubject('bank');
 
-  constructor(private route: ActivatedRoute, private cosmosSDK: CosmosSDKService, private configService: ConfigService) {
-    this.txTypeOptions = this.configService.config.extension?.messageModules
+  constructor(
+    private route: ActivatedRoute,
+    private cosmosSDK: CosmosSDKService,
+    private configService: ConfigService,
+  ) {
+    this.txTypeOptions = this.configService.config.extension?.messageModules;
     const timer$ = timer(0, this.pollingInterval * 1000);
     // eslint-disable-next-line no-unused-vars
     const sdk$ = timer$.pipe(mergeMap((_) => this.cosmosSDK.sdk$));
@@ -44,37 +48,34 @@ export class TxsComponent implements OnInit {
             console.error(error);
             return BigInt(0);
           });
-      })
+      }),
     );
-    this.initialTxs$ = combineLatest([
-      sdk$,
-      this.selectedTxType$,
-      this.txsTotalCount$,
-    ]).pipe(
-      mergeMap(([sdk, selectedTxType, txsTotalCount]) => {
-        const pageSize = BigInt(100);
-        const Offset = txsTotalCount - pageSize;
-        if (Offset <= 0) {
-          return [];
-        }
-        return rest.cosmos.tx
-          .getTxsEvent(
-            sdk.rest,
-            [`message.module='${selectedTxType}'`],
-            undefined,
-            Offset,
-            pageSize,
-            true,
-          )
-          .then((res) => {
-            return res.data.tx_responses
-          })
-          .catch((error) => {
-            console.error(error);
+    this.initialTxs$ = combineLatest([sdk$, this.selectedTxType$, this.txsTotalCount$])
+      .pipe(
+        mergeMap(([sdk, selectedTxType, txsTotalCount]) => {
+          const pageSize = BigInt(100);
+          const Offset = txsTotalCount - pageSize;
+          if (Offset <= 0) {
             return [];
-          });
-      })
-    )
+          }
+          return rest.cosmos.tx
+            .getTxsEvent(
+              sdk.rest,
+              [`message.module='${selectedTxType}'`],
+              undefined,
+              Offset,
+              pageSize,
+              true,
+            )
+            .then((res) => {
+              return res.data.tx_responses;
+            })
+            .catch((error) => {
+              console.error(error);
+              return [];
+            });
+        }),
+      )
       .pipe(map((latestTxs) => latestTxs?.reverse()));
 
     /*
@@ -114,7 +115,7 @@ export class TxsComponent implements OnInit {
     */
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   appSelectedTxTypeChanged(selectedTxType: string): void {
     this.selectedTxType$.next(selectedTxType);
