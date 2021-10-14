c4166e1cc09c333da0a9d5aec60df18c189336d0
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-10-14 10:24:56 +0900

    fix: remove unnecessary code and

--- projects/main/src/app/pages/home/txs/txs.component.ts
+++ projects/main/src/app/pages/home/txs/txs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { rest, websocket } from 'cosmos-client';
+import { rest } from 'cosmos-client';
 import { CosmosTxV1beta1GetTxsEventResponseTxResponses } from 'cosmos-client/esm/openapi/api';
 import { ConfigService } from 'projects/main/src/app/models/config.service';
 import { CosmosSDKService } from 'projects/main/src/app/models/cosmos-sdk.service';
@@ -15,7 +15,6 @@ import { map, mergeMap } from 'rxjs/operators';
 export class TxsComponent implements OnInit {
   pollingInterval = 30;
   initialTxs$?: Observable<CosmosTxV1beta1GetTxsEventResponseTxResponses[] | undefined>;
-  latestTxs$?: Observable<websocket.RequestSchema[] | websocket.ResponseSchema[]>;
   txTypeOptions?: string[];
   txsTotalCount$: Observable<bigint>;
   selectedTxType$: BehaviorSubject<string> = new BehaviorSubject('bank');
@@ -27,7 +26,6 @@ export class TxsComponent implements OnInit {
   ) {
     this.txTypeOptions = this.configService.config.extension?.messageModules;
     const timer$ = timer(0, this.pollingInterval * 1000);
-    // eslint-disable-next-line no-unused-vars
     const sdk$ = timer$.pipe(mergeMap((_) => this.cosmosSDK.sdk$));
 
     this.txsTotalCount$ = combineLatest([sdk$, this.selectedTxType$]).pipe(
@@ -77,42 +75,6 @@ export class TxsComponent implements OnInit {
         }),
       )
       .pipe(map((latestTxs) => latestTxs?.reverse()));
-
-    /*
-    this.cosmosSDK.websocketURL$.pipe(first()).subscribe((websocketURL) => {
-      const ws = websocket.connect(websocketURL);
-      ws.next({
-        id: '1',
-        jsonrpc: '2.0',
-        method: 'subscribe',
-        params: [`tm.event = 'Tx'`],
-      });
-
-      //暫定的に自動削除用のコードを無効化
-      this.initialTxs$ = initial;
-      // websocket有効化後、自動削除を有効に
-      this.initialTxs$ = combineLatest([initial, ws.asObservable()]).pipe(
-        map(([init, latest]) => {
-          (latest as websocket.ResponseSchema).result.data !== undefined
-            ? (init?.shift()) as CosmosTxV1beta1GetTxsEventResponseTxResponses[]
-            : init;
-          return init;
-        }),
-      );
-
-      this.latestTxs$ = ws.asObservable().pipe(
-        scan<any>((all, current) => [current, ...all], []),
-        map((data) => {
-          data[0].result.data === undefined ? data.pop() : data;
-          return data;
-        }),
-        map((data) => {
-          data.length > 20 ? data.pop() : data;
-          return data;
-        }),
-      );
-    });
-    */
   }
 
   ngOnInit(): void {}
--- projects/main/src/app/views/home/txs/txs.component.ts
+++ projects/main/src/app/views/home/txs/txs.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { websocket } from 'cosmos-client';
 import { CosmosTxV1beta1GetTxsEventResponseTxResponses } from 'cosmos-client/esm/openapi';
 
 @Component({
@@ -11,20 +10,18 @@ export class TxsComponent implements OnInit {
   @Input()
   initialTxs?: CosmosTxV1beta1GetTxsEventResponseTxResponses[] | undefined | null;
   @Input()
-  latestTxs?: websocket.ResponseSchema[] | undefined | null;
-  @Input()
   txTypeOptions?: string[] | undefined | null;
   @Input()
   selectedTxType?: string | undefined | null;
 
   @Output()
-  selectedTxTypeChanged: EventEmitter<string> = new EventEmitter<string>()
+  selectedTxTypeChanged: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   onSelectedTxTypeChanged(selectedTxType: string): void {
-    this.selectedTxTypeChanged.emit(selectedTxType)
+    this.selectedTxTypeChanged.emit(selectedTxType);
   }
 }
