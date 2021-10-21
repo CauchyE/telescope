48e9c8020d024f99108b57bb86d55b6afab591e5
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-10-21 16:18:38 +0900

    feat: fix-monitor

--- projects/main/src/app/models/monitor.service.ts
+++ projects/main/src/app/models/monitor.service.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from './config.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 export type Data = {
   before_date: string;
@@ -14,16 +15,14 @@ export type Data = {
 export class MonitorService {
   constructor(private readonly http: HttpClient, private readonly config: ConfigService) {}
 
-  list(year: number, month: number, day: number, count: number) {
-    return this.http
-      .get<Data[]>(`${this.config.config.extension?.monitor?.monitorURL}/list`, {
-        params: {
-          start_year: year,
-          start_month: month,
-          start_day: day,
-          count: count,
-        },
-      })
-      .toPromise();
+  list(year: number, month: number, day: number, count: number): Observable<Data[]> {
+    return this.http.get<Data[]>(`${this.config.config.extension?.monitor?.monitorURL}/list`, {
+      params: {
+        start_year: year,
+        start_month: month,
+        start_day: day,
+        count: count,
+      },
+    });
   }
 }
