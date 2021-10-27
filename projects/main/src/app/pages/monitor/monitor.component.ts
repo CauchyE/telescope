2f0bae72c6070f3c0f1fc5ace2c6398705a07676
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-10-27 22:08:27 +0900

    fix: daterange count

--- projects/main/src/app/pages/monitor/monitor.component.ts
+++ projects/main/src/app/pages/monitor/monitor.component.ts
@@ -2,7 +2,7 @@ import { MonitorService, Data } from '../../models/monitor.service';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
-import { BehaviorSubject, combineLatest, forkJoin, Observable, of } from 'rxjs';
+import { BehaviorSubject, combineLatest, forkJoin, Observable, of, zip } from 'rxjs';
 import { catchError, map, mergeMap, tap } from 'rxjs/operators';
 
 @Component({
@@ -12,9 +12,10 @@ import { catchError, map, mergeMap, tap } from 'rxjs/operators';
 })
 export class MonitorComponent implements OnInit {
   dataArray$: Observable<Data[]>;
-  startDate$: BehaviorSubject<Date>;
-  endDate$: BehaviorSubject<Date>;
-  count$: Observable<number>;
+  dateRange$: BehaviorSubject<[Date, Date]>;
+  startDate$: Observable<Date>;
+  endDate$: Observable<Date>;
+  // count$: Observable<number>;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,27 +23,30 @@ export class MonitorComponent implements OnInit {
     private snackBar: MatSnackBar,
   ) {
     const now = new Date();
-    this.endDate$ = new BehaviorSubject(now);
     const yesterday = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1);
-    this.startDate$ = new BehaviorSubject(yesterday);
-    const startDateMinus1$ = this.startDate$.asObservable().pipe(
-      map((start) => {
-        start.setDate(start.getDate() - 1);
-        return start;
+    this.dateRange$ = new BehaviorSubject([yesterday, now]);
+    const dateRangeMinus1$ = this.dateRange$;
+    //.asObservable().pipe(
+    //   map(([start, end]) => {
+    //     start.setDate(start.getDate() - 1);
+    //     end.setDate(end.getDate() - 1);
+    //     return [start, end];
+    //   }),
+    // );
+    this.startDate$ = this.dateRange$.pipe(map(([start, end]) => start));
+    this.endDate$ = this.dateRange$.pipe(map(([start, end]) => end));
+    // this.count$ = combineLatest([startDateMinus1$, endDateMinus1$]).pipe(
+    //   map(([start, end]) =>
+    //     Math.ceil((end.getMilliseconds() - start.getMilliseconds()) / 86400000),
+    //   ),
+    // );
+    this.dataArray$ = dateRangeMinus1$.pipe(
+      map(([start, end]) => {
+        console.log(start, end);
+        const count = Math.ceil(((end as any) - (start as any)) / 86400000) + 1;
+        console.log('count', count);
+        return [start, end, count] as [Date, Date, number];
       }),
-    );
-    const endDateMinus1$ = this.endDate$.asObservable().pipe(
-      map((end) => {
-        end.setDate(end.getDate() - 1);
-        return end;
-      }),
-    );
-    this.count$ = combineLatest([startDateMinus1$, endDateMinus1$]).pipe(
-      map(([start, end]) =>
-        Math.ceil((end.getMilliseconds() - start.getMilliseconds()) / 86400000),
-      ),
-    );
-    this.dataArray$ = combineLatest([startDateMinus1$, endDateMinus1$, this.count$]).pipe(
       mergeMap(([start, end, count]) =>
         this.monitor
           .list(start.getFullYear(), start.getMonth() + 1, start.getDate(), count)
@@ -50,7 +54,7 @@ export class MonitorComponent implements OnInit {
       ),
       map(([start, end, list]) =>
         list.filter(
-          (v) => start < new Date(Date.parse(v.date)) && new Date(Date.parse(v.date)) < end,
+          (v) => start <= new Date(Date.parse(v.date)) && new Date(Date.parse(v.date)) <= end,
         ),
       ),
       catchError((err) => {
@@ -65,8 +69,7 @@ export class MonitorComponent implements OnInit {
   ngOnInit(): void {}
 
   appSearchCriteriaChanged(event: { startDate: Date; endDate: Date }): void {
-    this.startDate$.next(event.startDate);
-    this.endDate$.next(event.endDate);
+    this.dateRange$.next([event.startDate, event.endDate]);
     console.log('start', event.startDate);
     console.log('end', event.endDate);
   }
