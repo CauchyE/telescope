8950dae360219fd9751b88486c81b268876a47f3
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-12-09 09:41:08 +0900

    chore: remove unnecessary comment

--- projects/main/src/app/pages/home/blocks/blocks.component.ts
+++ projects/main/src/app/pages/home/blocks/blocks.component.ts
@@ -60,36 +60,6 @@ export class BlocksComponent implements OnInit {
         return of(undefined);
       }),
     );
-    /*
-    this.cosmosSDK.websocketURL$.pipe(first()).subscribe((websocketURL) => {
-      const ws = websocket.connect(websocketURL);
-      ws.next({
-        id: '1',
-        jsonrpc: '2.0',
-        method: 'subscribe',
-        params: [`tm.event = 'NewBlock'`],
-      });
-
-      this.latestBlocks$ = ws.asObservable().pipe(
-        scan<any>((all, current) => [current, ...all], []),
-        map((data) => {
-          data[0].result.data === undefined ? data.pop() : data;
-          return data;
-        }),
-        map((data) => {
-          data.length > 20 ? data.pop() : data;
-          return data;
-        }),
-      );
-
-      //暫定的に自動削除用のコードを無効化
-      this.initialBlocks$ = initial;
-      // websocket有効化後、自動削除を有効に
-      this.initialBlocks$ = combineLatest([initial, this.latestBlocks$]).pipe(
-        map(([init, latest]) => latest.length === 20 ? undefined : init)
-      );
-    });
-    */
   }
 
   ngOnInit(): void {}
