b910ae787792bb5221c630e477aa6cfb7c2e75ed
taro04 <75844498+taro04@users.noreply.github.com>
2022-01-13 22:11:04 +0900

    fix: debug

--- projects/main/src/app/pages/blocks/blocks.component.ts
+++ projects/main/src/app/pages/blocks/blocks.component.ts
@@ -34,6 +34,7 @@ export class BlocksComponent implements OnInit {
     this.latestBlock$ = sdk$.pipe(
       mergeMap((sdk) => rest.tendermint.getLatestBlock(sdk.rest).then((res) => res.data)),
     );
+
     this.latestBlock$.subscribe((latestBlock) => {
       this.latestBlockHeight$.next(
         latestBlock?.block?.header?.height ? BigInt(latestBlock.block.header.height) : BigInt(0),
@@ -47,15 +48,22 @@ export class BlocksComponent implements OnInit {
       this.pageNumber$.next(0);
     });
 
+    //add
     this.route.queryParams.subscribe((params) => {
       console.log('pages', params); // { order: "popular" }
 
-      this.pageSize$.next(params.perPage);
+      console.log(this.pageSizeOptions.includes(Number(params.perPage)));
+      if (this.pageSizeOptions.includes(Number(params.perPage))) {
+        console.log('inclide', params.perPage);
+        this.pageSize$.next(params.perPage);
+      }
       this.pageNumber$.next(params.pages);
       console.log(this.pageSize$.getValue(), this.pageNumber$.getValue());
 
       const paginatedBlockHeight =
-        this.latestBlockHeight$.getValue() - BigInt(params.pages - 1) * BigInt(params.perPage);
+        this.latestBlockHeight$.getValue() - BigInt(params.pages - 1) * BigInt(params.perPage) > 0
+          ? this.latestBlockHeight$.getValue() - BigInt(params.pages - 1) * BigInt(params.perPage)
+          : BigInt(100);
       console.log('paginatedBlockHeight', paginatedBlockHeight);
 
       this.firstBlockHeight$.next(paginatedBlockHeight);
@@ -64,6 +72,7 @@ export class BlocksComponent implements OnInit {
     this.latestBlocks$ = combineLatest([this.firstBlockHeight$, this.pageSize$]).pipe(
       map(([firstBlockHeight, pageSize]) =>
         [...Array(pageSize).keys()].map((index) => {
+          console.log('index', index);
           const tempLatestBlockHeight =
             firstBlockHeight === undefined ? BigInt(0) : firstBlockHeight;
           return tempLatestBlockHeight - BigInt(index);
@@ -85,13 +94,24 @@ export class BlocksComponent implements OnInit {
         return of(undefined);
       }),
     );
+
+    this.pageSize$.subscribe((x) => console.log('pageSize', x));
+    this.firstBlockHeight$.subscribe((x) => console.log('1stBlockH', x));
   }
 
   ngOnInit(): void {}
 
   appPaginationChanged(pageEvent: PageEvent): void {
+    /*
+    this.pageSize$.next(pageEvent.pageSize);
+    this.pageNumber$.next(pageEvent.pageIndex + 1);
     this.pageLength$.next(pageEvent.length);
+    const paginatedBlockHeight =
+      this.latestBlockHeight$.getValue() - BigInt(pageEvent.pageIndex) * BigInt(pageEvent.pageSize);
+    this.firstBlockHeight$.next(paginatedBlockHeight);
+    */
 
+    //add
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams: {
